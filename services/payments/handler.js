08b4277e0660e7671f396ea11fb80ba95f915bfe
Renan Pupin <8743530+renanpupin@users.noreply.github.com>
2022-05-27 18:56:40 +0200

    monorepo

--- services/payments/handler.js
+++ services/payments/handler.js
@@ -1,8 +1,11 @@
+import * as DateLib from '../../libs/date.js';
+
 export const hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
+      date: DateLib.getCurrentTime(),
     }),
   };
 };
