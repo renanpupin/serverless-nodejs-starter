3b1fccd39284f1dd168f691ab2aa41896dffede4
Renan Pupin <8743530+renanpupin@users.noreply.github.com>
2022-05-30 17:46:51 +0200

    test

--- services/payments/handler.js
+++ services/payments/handler.js
@@ -1,4 +1,5 @@
 import * as DateLib from '../../libs/date.js';
+import samplePackage from 'sample-package';
 
 export const hello = async (event, context) => {
   return {
@@ -6,6 +7,7 @@ export const hello = async (event, context) => {
     body: JSON.stringify({
       message: `Go Serverless v2.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
       date: DateLib.getCurrentTime(),
+      samplePackageResponse: samplePackage(),
     }),
   };
 };
